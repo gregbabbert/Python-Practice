2d4e7290ec1cc1e13487f0e5b7e5c56f54cbdbcd
Greg Babbert <gdbabbert@gmail.com>
2023-09-05 23:38:10 -0400

    created function

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,2 +1,8 @@
-player_choice = 'rock'
-computer_choice = 'paper'
+def get_choices():
+    player_choice = 'rock'
+    computer_choice = 'paper'
+
+    return computer_choice
+
+choices = get_choices()
+print(choices)
