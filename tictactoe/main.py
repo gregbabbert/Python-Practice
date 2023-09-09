64229436fb8fbc6acfa9ec936f257b2f6389d2ed
Greg Babbert <gdbabbert@gmail.com>
2023-09-09 18:14:41 -0400

    worked on new function and if statement

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,10 +1,17 @@
-def get_choices():
-    player_choice = 'rock'
-    computer_choice = 'paper'
+import random
 
-    return computer_choice
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    possible_choices = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(possible_choices)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_win(player, computer):
+    if (player == computer):
+        return "It's a tie!"
+    
+    return player
 
 choices = get_choices()
 print(choices)
-
-dict = {"test": "test"}
