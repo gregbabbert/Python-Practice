326525fb7094af85e567f17a7681fc2fe48a7e41
Greg Babbert <gdbabbert@gmail.com>
2023-09-10 19:01:53 -0400

    wrote notes from lesson

---
+++ lesson/index.py
@@ -0,0 +1,23 @@
+# This is a comment
+
+name = "Sarah" # This is an inline comment
+print(name)
+print(type(name))
+print(type(name) == str) # prints true because name is a string
+print(isinstance(name, str)) # prints true because name is a string
+
+# other types include: int, float
+
+# You can force an int to be a float with
+age = float(25)
+
+# You can force a variable to be an int with
+ageInt = int("25")
+
+# complex for complex numbers
+# bool for booleans
+# list for lists
+# tuple for tuples
+# range for ranges
+# dict for dictionaries
+# set for sets
