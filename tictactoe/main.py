14141913edac6f0200b8895b89837d05d29140fc
Greg Babbert <gdbabbert@gmail.com>
2023-09-10 18:37:38 -0400

    finished rock paper scissors game

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -8,10 +8,29 @@ def get_choices():
     return choices
 
 def check_win(player, computer):
+    print("You chose " + player)
+    print(f"The computer chose {computer}")
     if (player == computer):
         return "It's a tie!"
-    
-    return player
+    elif player == "rock" and computer != "rock":
+        if computer == 'scissors':
+            return "You win!"
+        else:
+            return "You lose"
+    elif player == "paper":
+        if computer == 'rock':
+            return "You win!"
+        else:
+            return "You lose"
+    elif player == "scissors":
+        if computer == 'paper':
+            return "You win!"
+        else:
+            return "You lose"
+    else:
+        return "You chose an option we don't recognize"
 
 choices = get_choices()
 print(choices)
+win = check_win(choices["player"], choices["computer"])
+print(win)
